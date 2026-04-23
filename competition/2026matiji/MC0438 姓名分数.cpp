78ea6e8452d87930a834d0f5e4a334b1ace2e359
noxargenta <1849743809@qq.com>
2026-04-23 09:17:18 +0000

    2026年4月23日 17:17

--- competition/2026matiji/MC0438 姓名分数.cpp
+++ competition/2026matiji/MC0438 姓名分数.cpp
@@ -4,7 +4,27 @@ using i64 = long long;
 #define endl '\n'
 #define int long long
 void solve() {
-    
+    int n,m;
+    cin >> n ;
+    set<string> s;
+    for(int i=1;i<=n;i++){
+        string x;
+        cin   >> x;
+        s.insert(x);
+    }
+    cin >> m;
+    map<string,int> m;
+    for(int i=1;i<=m;i++){
+        int opt;
+        cin >> opt;
+        if(opt==1){
+            string x;
+            cin >> x;
+            int x2;
+            cin >> x2;
+            m[x]++;
+        }
+    }
 }
 
 signed main() {
