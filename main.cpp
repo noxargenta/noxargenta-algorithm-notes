d18681c745a72be012253ff750a244cc3db99c3f
noxargenta <1849743809@qq.com>
2026-04-26 13:07:36 +0800

    初始化项目结构，添加主程序框架

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+void solve(){
+    
+}
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _=1;
+    //cin >> _;
+    while(_--){
+        solve();
+    }
+}
