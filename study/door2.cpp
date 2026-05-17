38c49798e0b90df9ee40300fdff99006f22f957e
noxargenta <1849743809@qq.com>
2026-05-17 12:31:53 +0000

    2026年5月17日 20:31

--- study/door2.cpp
+++ study/door2.cpp
@@ -3,18 +3,25 @@
 #define endl '\n'
 using namespace std;
 vector<int> a(10,2021);
+int ok=0;
 int cf(int x){
     int ge=0;
     while(x!=0){
         ge=x%10;
         x/=10;
         a[ge]--;
+        if(a[ge]==-1){
+            ok=1;
+        }
     }
 }
 void solve(){
     for(int i=1;;i++){
         cf(i);
-        
+        if(ok==1){
+            cout << i << endl;
+            break;
+        }
     }
 }
 signed main(){
