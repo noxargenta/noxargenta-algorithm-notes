775e318573d2681d2bc30425d04986f4d7527eaa
noxargenta <1849743809@qq.com>
2026-05-17 08:43:40 +0000

    2026年5月17日 16:43

--- study/door2.cpp
+++ study/door2.cpp
@@ -2,8 +2,17 @@
 #define int long long 
 #define endl '\n'
 using namespace std;
+vector<int> a(10,2021);
+int cf(int x){
+    int ge=0;
+    while(x!=0){
+        ge=x%10;
+        x/=10;
+    }
+    return 
+}
 void solve(){
-    
+
 }
 signed main(){
     ios::sync_with_stdio(false);
