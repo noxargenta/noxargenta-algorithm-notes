82b92cac4580d7bc5b8257252d6b0037c7ceabdf
noxargenta <1849743809@qq.com>
2026-05-19 08:36:58 +0000

    2026年5月19日 16:36

--- study/Codeforces Round 1090 (Div. 4)/D_The_67_th_OEIS_Problem.cpp
+++ study/Codeforces Round 1090 (Div. 4)/D_The_67_th_OEIS_Problem.cpp
@@ -12,7 +12,7 @@ bool isPrime(int x){
     if(x<2){
         return 0;
     }else {
-        for(int i=1;i*i<=x;i++){
+        for(int i=2;i*i<=x;i++){
             if(x%i==0){
                 return 0;
             }
