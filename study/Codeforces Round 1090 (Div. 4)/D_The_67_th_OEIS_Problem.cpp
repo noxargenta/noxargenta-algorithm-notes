8293e83d5d2546e68b74959c1273740ae8a916cc
noxargenta <1849743809@qq.com>
2026-05-19 08:29:28 +0000

    2026年5月19日 16:29

--- study/Codeforces Round 1090 (Div. 4)/D_The_67_th_OEIS_Problem.cpp
+++ study/Codeforces Round 1090 (Div. 4)/D_The_67_th_OEIS_Problem.cpp
@@ -3,19 +3,23 @@ using namespace std;
 using i64 = long long;
 #define endl '\n'
 #define int long long
-
+int n;
+int m=0;
+int N=
+int p[]
 bool isPrime(int x){
     if(x<2){
         return 0;
     }else {
         for(int i=1;i*i<=x;i++){
-            if()
+            if(x%i==0){
+                return 0;
+            }
         }
+        return 1;
     }
-
 }
 void solve() {
-    int n;
     cin >> n;
 
 }
