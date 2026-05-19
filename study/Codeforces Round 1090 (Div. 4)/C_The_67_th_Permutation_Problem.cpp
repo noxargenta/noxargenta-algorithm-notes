b5cad03c815b842bee6ff54cbe6f6e8bc4fdf657
noxargenta <1849743809@qq.com>
2026-05-19 05:27:50 +0000

    2026年5月19日 13:27

--- study/Codeforces Round 1090 (Div. 4)/C_The_67_th_Permutation_Problem.cpp
+++ study/Codeforces Round 1090 (Div. 4)/C_The_67_th_Permutation_Problem.cpp
@@ -7,9 +7,20 @@ void solve() {
     int n;
     cin >> n;
     vector<int> a(3*n+1);
-    for(int i=n+1,jiou=(n+1)%2,l=1,r=2;i<=3*n;i+=2){
-        a[]
+    for(int i=1,j=1;i<=n;i++,j+=3){
+        a[j]=i;
     }
+    for(int i=n+1,j=2;i<=3*n;i+=2,j+=3){
+        a[j]=i;
+        a[j+1]=i+1;
+    }
+    for(auto x : a ){
+        if(x==0){
+            continue;
+        }
+        cout <<  x << ' ' ;
+    }
+    cout << endl ;
 }
 
 signed main() {
