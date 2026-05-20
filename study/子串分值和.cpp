f6c71a28a549c9d1deb36f4850f0bcb519c46637
noxargenta <1849743809@qq.com>
2026-05-20 09:15:18 +0000

    2026年5月20日 17:15

---
+++ study/子串分值和.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    string s;
+    int last[26];
+    cin >> s;
+    int sum=0;
+    fill(last+1,last+26,0);
+    for(int i=0;i<s.length();i++){
+        
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
