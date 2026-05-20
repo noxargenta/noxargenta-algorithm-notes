fb93619499e3c3c82e65316ed5440598d9639feb
noxargenta <1849743809@qq.com>
2026-05-20 08:25:53 +0000

    2026年5月20日 16:25

---
+++ study/P_5733_深基_6_例_1_自动修正.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    string s;
+    cin  >> s;
+    for(int i=0;i<s.length();i++){
+        if(s[i]>='Z' || s[i] <= 'A'){
+            continue;
+        }else 
+        if(s[i]<'z' && s[i] > 'a'){
+            s[i]=s[i]+'A'-'a';
+        }
+    }
+    cout << s << endl;
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
