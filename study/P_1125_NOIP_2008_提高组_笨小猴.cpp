dcbc816cbb41813c54e9c951818e90647e9fddc5
noxargenta <1849743809@qq.com>
2026-05-20 08:43:45 +0000

    2026年5月20日 16:43

--- study/P_1125_NOIP_2008_提高组_笨小猴.cpp
+++ study/P_1125_NOIP_2008_提高组_笨小猴.cpp
@@ -4,17 +4,33 @@ using i64 = long long;
 #define endl '\n'
 #define int long long
 int isPrime(int x){
-    
+    if(x<2){
+        return 0;
+    }
         for(int i=2;i*i<=x;i++){
             if(x%i==0){
                 return 0;
             }
         }
     return 1;
-    
 }
 void solve() {
-    
+    vector<int> a(26,0);
+    string s;
+    cin >> s;
+    int maxn=0;
+    int minn=10000;
+    for(int i=1;i<s.length();i++){
+        int x=++a[s[i]-'a'];
+        minn=min(minn,x);
+        maxn=max(maxn,x);
+    }
+    if(isPrime(maxn-minn)){
+        cout  << "Lucky Word\n";
+        cout << maxn-minn << endl;
+    }else {
+        cout << "No Answer\n0";
+    }
 }
 
 signed main() {
