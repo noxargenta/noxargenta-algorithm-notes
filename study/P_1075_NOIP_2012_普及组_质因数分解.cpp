4fad8b3f049f365a081e6d5d8a6aadf3eae5a771
noxargenta <1849743809@qq.com>
2026-05-20 11:52:26 +0000

    2026年5月20日 19:52

---
+++ study/P_1075_NOIP_2012_普及组_质因数分解.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+bool isPrime(int x){
+    if(x<2){
+        return 0;
+    }
+    for(int i=2;i*i<=x;i++){
+        if(x%i==0){
+            return 0;
+        }
+    }
+    return 1;
+}
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a;
+    for(int i=2;i*i<=2*1e9;i++){
+        if(isPrime(i)&& n%i==0){
+            if(isPrime(n/i)){
+                cout << n/i << endl;
+                return ;
+            }
+        }
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
