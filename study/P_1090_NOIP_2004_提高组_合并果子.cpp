e06da692542ef8595e905cd46031c45d2b065057
noxargenta <1849743809@qq.com>
2026-05-21 20:06:28 +0800

    贪心复习lanbada表达式

---
+++ study/P_1090_NOIP_2004_提高组_合并果子.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    int n;
+    cin >> n;
+    priority_queue<int,vector<int>,greater<int>> a;
+    while(n--){
+        int t;
+        cin >> t;
+
+        a.push(t);
+
+    } 
+    int sum=0;
+    while(a.size()!=1){
+        int x,y;
+        x=a.top();
+        a.pop();
+        y=a.top();
+        a.pop();
+        a.push(x+y);
+        sum+=x+y;
+    }
+    cout <<sum << endl;
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
