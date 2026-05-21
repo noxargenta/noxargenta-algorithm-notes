beab768dd0f25076821cc71a3b308b5a836cb746
noxargenta <1849743809@qq.com>
2026-05-21 14:57:51 +0000

    2026年5月21日 22:57

--- competition/Codeforces Round 1099 (Div. 2)/F_Quadratic_Jumps.cpp
+++ competition/Codeforces Round 1099 (Div. 2)/F_Quadratic_Jumps.cpp
@@ -11,26 +11,34 @@ void solve() {
     int last=2*n;
     int sum=1;
     s.insert(last);
+    vector<int> a;
     while(x*=-1 && sum!=n){
         int r=2*n-1;
         int l=1;
         if(x==-1){
-            if(!s.count(l)){
+            if(!s.count(l) && !s.count(r+last)){
                 s.insert(l);
+                a.push_back(l);
                 sum++;
                 s.insert(l+last);
                 last=l;
             }
             l++;
         }else {
-            if(!s.count(r)){
+            if(!s.count(r) && !s.count(r+last)){
                 s.insert(r);
+                a.push_back(r);
                 s.insert(r+last);
+                sum++;
                 last=r;
             }
             r--;
         }
     }
+    for(auto x : a){
+        cout << x  << ' ';
+    }
+    cout << endl;
 }
 
 signed main() {
