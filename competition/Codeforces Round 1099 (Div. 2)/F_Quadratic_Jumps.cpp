4eb0407f6e007d6d30a52d2c03acd31a12ddce5f
noxargenta <1849743809@qq.com>
2026-05-21 15:11:58 +0000

    2026年5月21日 23:11

--- competition/Codeforces Round 1099 (Div. 2)/F_Quadratic_Jumps.cpp
+++ competition/Codeforces Round 1099 (Div. 2)/F_Quadratic_Jumps.cpp
@@ -17,8 +17,8 @@ void solve() {
             break;
         }
     }
-    for(auto x : a){
-        cout << x  << ' ';
+    for(int i=1;i<=n;i++){
+        cout << a[i] << ' ';
     }
     cout << endl;
 }
