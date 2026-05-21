2f292743d16cbe0263afa508bd11530dc37ad09c
noxargenta <1849743809@qq.com>
2026-05-21 15:28:37 +0000

    2026年5月21日 23:28

--- competition/Codeforces Round 1099 (Div. 2)/B_Another_Sorting_Problem.cpp
+++ competition/Codeforces Round 1099 (Div. 2)/B_Another_Sorting_Problem.cpp
@@ -4,15 +4,21 @@ using i64 = long long;
 #define endl '\n'
 #define int long long
 void solve() {
+    int n;
+    cin  >> n;
+    vector<int> a(n+1);
+    for(int i=1;i<=n;i++){
+        cin  >> a[i];
+    }
     
-}
+}    
 
 signed main() {
 //  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int _ = 1;
-    // cin >> _;
+    cin >> _;
     while(_--) {
         solve();
     }
