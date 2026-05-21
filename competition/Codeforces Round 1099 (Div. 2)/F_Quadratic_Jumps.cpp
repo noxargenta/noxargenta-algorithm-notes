03c960917036cda67a0356cd64219e2f991eb0c6
noxargenta <1849743809@qq.com>
2026-05-21 15:05:08 +0000

    2026年5月21日 23:05

--- competition/Codeforces Round 1099 (Div. 2)/F_Quadratic_Jumps.cpp
+++ competition/Codeforces Round 1099 (Div. 2)/F_Quadratic_Jumps.cpp
@@ -4,37 +4,14 @@ using i64 = long long;
 #define endl '\n'
 #define int long long
 void solve() {
-    int n;
-    cin >> n;
-    set<int> s;
-    int x=1;
-    int last=2*n;
-    int sum=1;
-    s.insert(last);
     vector<int> a;
-    a.push_back(last);
-    int r=2*n-1;
-    int l=1;
-    while(sum!=n){
-        x*=-1;
-        if(x==-1){
-            if(!s.count(l) && !s.count(r+last)){
-                s.insert(l);
-                a.push_back(l);
-                sum++;
-                s.insert(l+last);
-                last=l;
-            }
-            l++;
-        }else {
-            if(!s.count(r) && !s.count(r+last)){
-                s.insert(r);
-                a.push_back(r);
-                s.insert(r+last);
-                sum++;
-                last=r;
-            }
-            r--;
+    int n;
+    cin  >> n;
+    set<int> s;
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=2*n;j++){
+
         }
     }
     for(auto x : a){
