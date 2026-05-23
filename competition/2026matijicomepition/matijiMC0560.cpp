a8ef7ef02e9f94b890476b2b756b07d40111d299
noxargenta <1849743809@qq.com>
2026-05-23 14:51:39 +0000

    2026年5月23日 22:51

---
+++ competition/2026matijicomepition/matijiMC0560.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+const int N=3050;
+vector<int> G[N];
+int dp[N][N];
+const int MOD=998244353;
+int n;
+void dfs(int u,int fa){
+    if(G[u].size()==1 && fa!=0){
+        dp[u][1]=1;
+        return;
+    }
+    for(auto v : G[u]){
+        if(v==fa)continue;
+        dfs(v,u);
+    }
+    for(int i=0;i<n;i++){
+        int a=1,b=1;
+        for(auto v:G[u]){
+            if(v==fa)continue;
+            a=a*dp[v][i+1]%MOD;
+            if(i>0){
+                b=b*dp[v][i-1]%MOD;
+            }
+        }
+        if(i==0){
+            dp[u][i]=a%MOD;
+        }else {
+            dp[u][i]=(a+b)%MOD;
+        }
+    }
+}
+void solve() {
+    cin >> n;
+    for(int i=0;i<N;i++){
+        G[i].clear();
+    }
+    for(int i=0;i<=n+2;i++){
+        for(int j=0;j<=n+2;j++){
+            dp[i][j]=0;
+        }
+    }
+    for(int i=1;i<n;i++){
+        int u,v;
+        cin >> u >> v;
+        G[u].push_back(v);
+        G[v].push_back(u);
+    }
+    if(n==1){
+        cout << 0 << endl;
+    }else {
+        dfs(1,0);
+        cout << dp[1][0] << endl;
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
