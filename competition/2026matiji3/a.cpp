5028b62775f1e3c3c41afcf28075ba0be8bea85a
noxargenta <1849743809@qq.com>
2026-05-24 08:01:22 +0000

    2026年5月24日 16:01

---
+++ competition/2026matiji3/a.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+void solve(){
+    
+}
+signed main(){
+    int _=1;
+    //cin >>_;
+    while(_--){
+        solve();
+    }
+    return ;
+}
