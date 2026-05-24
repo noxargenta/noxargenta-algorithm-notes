56925d9dcf6e9e090583a96239b955fa297389e8
noxargenta <1849743809@qq.com>
2026-05-24 08:16:42 +0000

    2026年5月24日 16:16

--- competition/2026matiji3/b.cpp
+++ competition/2026matiji3/b.cpp
@@ -12,7 +12,7 @@ void solve() {
     for(int i=1;i<=n;i++){
         cin >> a[i];
     }
-    for(int i=1;i<=n;i++){
+    for(int i=1;i<=m;i++){
         cin >> b[i];
         for(int j=1;j<=n;j++){
             int ok=1;
