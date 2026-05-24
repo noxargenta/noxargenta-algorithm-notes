096f50e761680e346106fc116472142cf3423439
noxargenta <1849743809@qq.com>
2026-05-24 08:59:18 +0000

    2026年5月24日 16:59

---
+++ competition/2026matiji3/d.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    int n,q;
+    cin >> n >> q;
+    vector<int> a(n+1);
+    for(int i=1;i<=n;i++){
+        cin >> a[i];
+    }
+    a[0]=-1;
+    for(int i=1;i<=q;i++){
+        int l,r;
+        cin >> l >>r;
+        int sum=0;
+        int las=-1;
+        int ok=0;
+        int maxx=0;
+        for(int j=l;j<=r;j++){
+            maxx=max(sum,maxx);
+            if(a[i]>a[i-1]){
+                sum++;
+            }else {
+                sum=1;
+            }
+            maxx=max(sum,maxx);
+        }
+        if(maxx>=3){
+            cout << "Yes\n";
+        }else {
+            cout << "No\n";
+        }
+    }
+    
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
