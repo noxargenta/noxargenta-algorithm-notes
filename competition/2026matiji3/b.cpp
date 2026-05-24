75e67f44f141f136f02d9ee5983cd4d50e81a55d
noxargenta <1849743809@qq.com>
2026-05-24 08:09:44 +0000

    2026年5月24日 16:09

--- competition/2026matiji3/b.cpp
+++ competition/2026matiji3/b.cpp
@@ -4,6 +4,21 @@ using i64 = long long;
 #define endl '\n'
 #define int long long
 void solve() {
+    int n,m;
+    cin >> n >> m;
+    vector<string> a(n+1);
+    vector<int> cnt(n+1);
+    
+    vector<string> b(n+1);
+    for(int i=1;i<=n;i++){
+        cin >> a[i];
+    }
+    for(int i=1;i<=n;i++){
+        cin >> b[i];
+        for(int j=1;j<=n;j++){
+            if(a[i].substr(a.begin(),b[i].size()))
+        }
+    }
     
 }
 
