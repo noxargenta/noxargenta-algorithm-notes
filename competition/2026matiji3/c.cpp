700c6976f841b0127d6b4aab413f1481e6ba110c
noxargenta <1849743809@qq.com>
2026-05-24 08:29:49 +0000

    2026年5月24日 16:29

---
+++ competition/2026matiji3/c.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    int n,m,t1,t2;
+    cin >> n >> m >> t1 >> t2;
+    int n2=n/t1;
+    int m2=m/t2;
+    int g[n+1][m+1];
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            cin >> g[i][j];
+        }
+    }
+    
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
