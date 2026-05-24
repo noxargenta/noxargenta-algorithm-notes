40d4d17a467d58f6c5d1912c15d0b4014ef98ee5
noxargenta <1849743809@qq.com>
2026-05-24 10:04:31 +0000

    2026年5月24日 18:04

---
+++ competition/2026matiji3/6.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    int n,m;
+    cin >> n >> m;
+    vector<int> a(n+1,1000000009);
+    while(m--){
+        int l,r,v;
+        cin >> l >> r >> v;
+        for(int i=l;i<=r;i++){
+            a[i]=min(a[i],v);
+        }
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
