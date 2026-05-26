6c0f66e5ab676ec880eecabb10784ef437e62476
noxargenta <1849743809@qq.com>
2026-05-26 11:22:07 +0000

    2026年5月26日 19:22

---
+++ study/P_1036_NOIP_2002_普及组_选数.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+bool isPrime(int x){
+    if(x<2){
+        return 0;
+    }else {
+        for(int i=2;i*i<=x;i++){
+            if(x%i==0){
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+int dfs(int u,int fa){
+
+}
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n+1);
+    for(int i=1;i<=n;i++){
+        cin >> a[i];
+    }
+    
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
