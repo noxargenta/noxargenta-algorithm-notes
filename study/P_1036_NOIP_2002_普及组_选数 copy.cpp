84828a411319edb168e759239835b23c60586b89
noxargenta <1849743809@qq.com>
2026-05-26 12:38:28 +0000

    2026年5月26日 20:38

---
+++ study/P_1036_NOIP_2002_普及组_选数 copy.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+vector<int> a(30);
+int n,cnt,kk;
+const int N=100000008;
+bitset<N> c;
+int b[200];
+
+//int vis[200];
+bool isPrime(int x){
+    if(x<2){
+        return 0;
+    }else {
+        for(int i=2;i*i<=x;i++){
+            if(x%i==0){
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+void dfs(int k,int u,int sum){
+    if(kk-(k-1) > n-u +1){
+        return;
+    }
+    if(k-1==kk){
+        // for(int i=1;i<=kk;i++){
+        //     sum+=b[i];
+        // }
+        if(c[sum]){
+            cnt++;
+        }
+        return;
+    }
+    for(int i=u;i<=n;i++){
+        //if(vis[i]==0){
+            //vis[i]=1;
+            //b[k]=a[i];
+            dfs(k+1,i+1,sum+a[i]);
+        //}
+    }
+};
+
+void solve() {
+    
+    cin >> n;
+    cin >> kk;
+    for(int i=1;i<=n;i++){
+        cin >> a[i];
+    }
+    dfs(1,1,0);
+    
+    cout << cnt << endl;
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    c.set();
+    c[0] = c[1] = 0;
+    
+    vector<int> primes;
+    for(int i=2;i<N;i++){
+        if(c[i]){
+            primes.push_back(i);
+        }
+        for(auto x : primes){
+            if(x * i>=N)break;
+            c[x * i]=0;
+            if(i % x==0)break;
+        }
+    }
+    
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
--- study/P_1036_NOIP_2002_普及组_选数.cpp
+++ study/P_1036_NOIP_2002_普及组_选数.cpp
@@ -3,13 +3,7 @@ using namespace std;
 using i64 = long long;
 #define endl '\n'
 #define int long long
-vector<int> a(30);
-int n,cnt,kk;
-const int N=100000008;
-bitset<N> c;
-int b[200];
-
-//int vis[200];
+int b[200],vis[200];
 bool isPrime(int x){
     if(x<2){
         return 0;
@@ -22,36 +16,34 @@ bool isPrime(int x){
     }
     return 1;
 }
-void dfs(int k,int u,int sum){
-    if(kk-(k-1) > n-u +1){
-        return;
-    }
-    if(k-1==kk){
-        // for(int i=1;i<=kk;i++){
-        //     sum+=b[i];
-        // }
-        if(c[sum]){
-            cnt++;
-        }
-        return;
-    }
-    for(int i=u;i<=n;i++){
-        //if(vis[i]==0){
-            //vis[i]=1;
-            //b[k]=a[i];
-            dfs(k+1,i+1,sum+a[i]);
-        //}
-    }
-};
 
 void solve() {
-    
+    int n;
     cin >> n;
-    cin >> kk;
+    int cnt=0;
+    vector<int> a(n+1);
     for(int i=1;i<=n;i++){
         cin >> a[i];
     }
-    dfs(1,1,0);
+    auto dfs = [&](int k,int u) -> void{
+        if(k==n){
+            int sum=0;
+            for(int i=1;i<=n;i++){
+                sum+=b[i];
+            }
+            if(isPrime(sum)){
+                cnt++;
+            }
+            return;
+        }
+        for(int i=u;i<=n;i++){
+            if(vis[i]==0){
+                vis[i]=1;
+                b[k]=a[i];
+                dfs(k+1,i+1);
+            }
+        }
+    };
     
     cout << cnt << endl;
 }
@@ -61,21 +53,6 @@ signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int _ = 1;
-    c.set();
-    c[0] = c[1] = 0;
-    
-    vector<int> primes;
-    for(int i=2;i<N;i++){
-        if(c[i]){
-            primes.push_back(i);
-        }
-        for(auto x : primes){
-            if(x * i>=N)break;
-            c[x * i]=0;
-            if(i % x==0)break;
-        }
-    }
-    
     // cin >> _;
     while(_--) {
         solve();
