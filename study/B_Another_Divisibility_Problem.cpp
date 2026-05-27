2f5a5c5caf6787fdc00a00680cfa40576dcb104f
noxargenta <1849743809@qq.com>
2026-05-27 11:36:39 +0000

    2026年5月27日 19:36

---
+++ study/B_Another_Divisibility_Problem.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    int t; cin >> t;
+    for (int i=0;i<t;i++) {
+        int x; cin >> x;
+        cout << 2*x << endl;
+    }
+    return 0;
+}
