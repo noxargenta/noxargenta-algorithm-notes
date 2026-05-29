9642ea415741f072e0c34b352cea4940aad29d7a
noxargenta <1849743809@qq.com>
2026-05-29 09:59:53 +0000

    2026年5月29日 17:59

--- study/P_3367_模板_并查集.cpp
+++ study/P_3367_模板_并查集.cpp
@@ -5,13 +5,28 @@ using i64 = long long;
 #define int long long
 const int N=2*1e5+2;
 int M;
+int f[N];
+int fa(int x){
+    return x==f[x] ? x : fa(f[x]);
+}   
 void solve() {
     int n,m;
     cin >> n  >> m;
+    for(int i=1;i<=n;i++){
+        f[i]=i;
+    }
     while(m--){
         int z,x,y;
-        cin >> z >> x > >y;
-        
+        cin >> z >> x >>y;
+        if(z==1){
+            int xf=fa(x);
+            int yf=fa(y);
+            f[xf]=yf;
+            f[yf]=yf;
+        }else {
+            if(f[x]==f[y])cout << "Y\n";
+            else cout << "N\n";
+        }
     }
 }
 
