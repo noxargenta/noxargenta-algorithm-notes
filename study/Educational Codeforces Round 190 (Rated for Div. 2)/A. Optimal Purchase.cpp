232ac02f6e7e382ccd5662dfa79be8b01dd73f85
noxargenta <1849743809@qq.com>
2026-05-31 02:49:03 +0000

    2026年5月31日 10:49

---
+++ study/Educational Codeforces Round 190 (Rated for Div. 2)/A. Optimal Purchase.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    int n,a,b;
+    cin >> n >> a >>b;
+    if(3*a<=b){
+        cout << n*a << endl;
+    }else if(){
+
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
