98e0d2b34943f7af93d3e8e077d97051a87bf975
noxargenta <1849743809@qq.com>
2026-06-01 12:17:56 +0000

    2026年6月1日 20:17

---
+++ study/P_1077_NOIP_2012_普及组_摆花.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+const int N=105,mod=1e6+7;
+int n,m;
+int a[N],dp[N][N][N];
+void solve() {
+    
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
