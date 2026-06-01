21dc3f10577e3c9566adfcae189844e33b25cfed
noxargenta <1849743809@qq.com>
2026-06-01 12:23:46 +0000

    2026年6月1日 20:23

--- study/P_1077_NOIP_2012_普及组_摆花.cpp
+++ study/P_1077_NOIP_2012_普及组_摆花.cpp
@@ -6,8 +6,18 @@ using i64 = long long;
 const int N=105,mod=1e6+7;
 int n,m;
 int a[N],dp[N][N][N];
+///////////1、多少种花  2、   3、
 void solve() {
-    
+    cin >> n;
+    dp[0][0][0]=1;
+    for(int i=1;i<=n;i++){
+        cin >> a[i];
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=a[i];j++){
+
+        }
+    }
 }
 
 signed main() {
