21c5081870f38920e65ebb5389540629aaf0f59e
noxargenta <1849743809@qq.com>
2026-06-04 12:46:33 +0000

    2026年6月4日 20:46

--- study/P_1060_NOIP_2006_普及组_开心的金明.cpp
+++ study/P_1060_NOIP_2006_普及组_开心的金明.cpp
@@ -4,7 +4,6 @@ using i64 = long long;
 #define endl '\n'
 #define int long long
 int n,m;
-
 void solve() {
     cin >> n >> m;
     int dp[m][n];
@@ -12,7 +11,20 @@ void solve() {
     for(int i=1;i<=m;i++){
         cin >> v[i] >> w[i];
     }    
-    
+    for(int i=0;i<=m;i++){
+        for(int j=0;j<=n;j++){
+            if(i==0 || j==0){
+                dp[i][j]=0;
+            }else {
+                if(j>=v[i]){
+                    dp[i][j]=max(dp[i-1][j],dp[i-1][j-v[i]]+v[i]*w[i]);
+                }else {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+    }
+    cout << dp[m][n] << endl;
 }
 
 signed main() {
