914a1b2cc9cb8e1c299c514413d88fb1d7969fe1
noxargenta <1849743809@qq.com>
2026-06-04 20:12:41 +0800

    添加 NOIP 2005 普及组 采药 题目及解决方案 添加 NOIP 2004 提高组 合唱队形 DP 解决方案

---
+++ study/P_1048_NOIP_2005_普及组_采药.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+int N,M;
+void solve() {
+    cin >> N >> M;
+    while(M--){
+        int x,y;
+        cin >> x >>y;
+        
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
