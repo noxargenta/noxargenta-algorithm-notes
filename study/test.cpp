fbb2196a29bdd6ce8f04fdb55fb8bce73a089356
noxargenta <1849743809@qq.com>
2026-06-05 15:16:53 +0000

    2026年6月5日 23:16

--- study/test.cpp
+++ study/test.cpp
@@ -3,6 +3,21 @@ using namespace std;
 using i64 = long long;
 #define endl '\n'
 #define int long long
+bitset<N> c;
+c.set();
+c[0]=c[1]=0;
+vector<int> prime;
+for(int i=2;i<N;i++){
+    if(c[i]==1){
+        prime.push_back(c[i]);
+    }else{
+        for(auto x : prime){
+            if(x*i>=N)break;
+            c[x*i]=0;
+            if()
+        }
+    }
+}
 int find(int x){
     return fa[x]==x ? x : fa[x]=find(fa[x]);
 }
