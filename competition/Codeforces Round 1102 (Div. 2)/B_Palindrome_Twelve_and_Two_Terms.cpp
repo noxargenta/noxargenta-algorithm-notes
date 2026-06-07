d60238faf16ccf342585104e97f31f103f8165af
noxargenta <1849743809@qq.com>
2026-06-07 15:22:35 +0000

    2026年6月7日 23:22

---
+++ competition/Codeforces Round 1102 (Div. 2)/B_Palindrome_Twelve_and_Two_Terms.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+int rev(int p,bool odd){
+    string s=to_string(p);
+    string s2=s;
+    reverse(s2.begin(),s2.end());
+    if(odd){
+        return stoll(s+)
+    }
+}
+void solve() {
+    
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
