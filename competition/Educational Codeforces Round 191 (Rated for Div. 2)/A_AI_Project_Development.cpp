626e64a24430509ea1b55b643e81d2928542546d
noxargenta <1849743809@qq.com>
2026-06-09 14:42:02 +0000

    2026年6月9日 22:42

--- competition/Educational Codeforces Round 191 (Rated for Div. 2)/A_AI_Project_Development.cpp
+++ competition/Educational Codeforces Round 191 (Rated for Div. 2)/A_AI_Project_Development.cpp
@@ -13,10 +13,11 @@ void solve() {
 //  ( 1≤n,x,y,z≤10000
 //  )，分别是项目中的行数、Maxim 的速度、Nikita 在没有 AI 的情况下的速度以及 AI 设置时间。
     if(n<=z*x){
-        cout << (n+1)/x << endl;
+        cout << (n+1)/(x+y) << endl;
     }else {
         cout << z + (n-z*x+1)/()
     }
+    cout << min((n+1)/(x+y),min())
 
 
 }
