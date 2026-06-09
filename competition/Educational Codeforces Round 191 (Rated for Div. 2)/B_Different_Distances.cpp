a19ee639e5ebfa8695828633f0ac109f9e8803f2
noxargenta <1849743809@qq.com>
2026-06-09 15:06:48 +0000

    2026年6月9日 23:06

---
+++ competition/Educational Codeforces Round 191 (Rated for Div. 2)/B_Different_Distances.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    vector<int> a={1,1,2,1,2,3,1,2,2,3,3,}
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
