dcd9148d5a6f4a840827477b5862a105a8893077
noxargenta <1849743809@qq.com>
2026-06-09 15:29:04 +0000

    2026年6月9日 23:29

--- competition/Educational Codeforces Round 191 (Rated for Div. 2)/B_Different_Distances copy.cpp
+++ competition/Educational Codeforces Round 191 (Rated for Div. 2)/B_Different_Distances copy.cpp
@@ -15,7 +15,9 @@ void solve() {
         for(int i=1;i<=n;i++){
             cout << i << " ";
         }
-        
+        for(int i=1;i<=n;i++){
+            cout << (i+1)%n << " ";
+        }
     }
 
 
