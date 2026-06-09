e52b737fc7d46369a71ed1afdad877f6fd769d83
noxargenta <1849743809@qq.com>
2026-06-09 14:38:25 +0000

    2026年6月9日 22:38

--- competition/Educational Codeforces Round 191 (Rated for Div. 2)/A_AI_Project_Development.cpp
+++ competition/Educational Codeforces Round 191 (Rated for Div. 2)/A_AI_Project_Development.cpp
@@ -4,7 +4,16 @@ using i64 = long long;
 #define endl '\n'
 #define int long long
 void solve() {
+    int n,x,y,z;
+    cin >> n >> x >> y >> z;
+//     //四个整数 n
+//  、 x
+//  、 y
+//  和 z
+//  ( 1≤n,x,y,z≤10000
+//  )，分别是项目中的行数、Maxim 的速度、Nikita 在没有 AI 的情况下的速度以及 AI 设置时间。
     
+
 }
 
 signed main() {
@@ -12,7 +21,7 @@ signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int _ = 1;
-    // cin >> _;
+    cin >> _;
     while(_--) {
         solve();
     }
