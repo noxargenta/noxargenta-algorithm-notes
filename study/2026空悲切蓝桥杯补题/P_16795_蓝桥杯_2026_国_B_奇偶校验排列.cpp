3ce3150236fff9aba6081325da9bcc27ec357265
noxargenta <1849743809@qq.com>
2026-07-07 05:57:01 +0000

    2026年7月7日 13:57

--- study/2026空悲切蓝桥杯补题/P_16795_蓝桥杯_2026_国_B_奇偶校验排列.cpp
+++ study/2026空悲切蓝桥杯补题/P_16795_蓝桥杯_2026_国_B_奇偶校验排列.cpp
@@ -4,20 +4,22 @@ using i64 = long long;
 #define endl '\n'
 #define int long long
 void solve() {
-    int n;
-    cin >> n;
+    int n,m;
+    cin >> n >> m;
     string s;
     cin >> s;
     int zero=0;
     vector<int> cnt(n+1,0);
-    for(auto x : s) {
-        if(x-'0'==0){
+    vector<int> pre(n+1,0);
+    for(int i=0;i<n;i++) {
+        if(s[i]-'0'==0){
             zero++;
         }else {
             int nowz=(zero+1)*zero/2;
             cnt[0]+=nowz;
             int ways=zero +1;
-            
+            zero=0;
+            for(int j=i;j<n && j<i+6;j++)
         }
     }
 }
