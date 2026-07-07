7fbb915b1d1575458ca8e8960d314a6ac339b9f2
noxargenta <1849743809@qq.com>
2026-07-07 05:20:20 +0000

    2026年7月7日 13:20

---
+++ study/2026空悲切蓝桥杯补题/P_16793_蓝桥杯_2026_国_B_玩具青蛙.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+//这题其实很简单直接裂项就好了，当时咋没想出来呢。
+void solve() {
+    cout << 2026 + 4053 << endl;
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
