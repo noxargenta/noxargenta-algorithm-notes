875f1f20217d4d3e63e377e4eb37f006dfa89174
noxargenta <1849743809@qq.com>
2026-07-09 06:54:36 +0000

    2026年7月9日 14:54

---
+++ study/2026空悲切蓝桥杯补题/P_16794_蓝桥杯_2026_国_B_生成派生码.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+const int a2=20260606,a1=20000000;
+void solve() {
+    set<int> se;
+    string s;
+    for(int i=)
+}   
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
