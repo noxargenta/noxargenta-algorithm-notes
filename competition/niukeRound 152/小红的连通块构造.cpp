5ca75f1297d37a9848cb48b33af48964757495d6
noxargenta <1849743809@qq.com>
2026-07-12 11:08:12 +0000

    2026年7月12日 19:08

---
+++ competition/niukeRound 152/小红的连通块构造.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    int n;
+    cin >> n;
+    if(n>21){
+        cout << -1 <<endl;
+        return;
+    }
+    int sum=0;
+    for()
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
