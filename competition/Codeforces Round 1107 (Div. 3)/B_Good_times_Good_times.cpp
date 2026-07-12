7adfd5ae6eba3edfa659c31ab53409cd94d8c0ad
noxargenta <1849743809@qq.com>
2026-07-12 09:19:50 +0000

    2026年7月12日 17:19

---
+++ competition/Codeforces Round 1107 (Div. 3)/B_Good_times_Good_times.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    int x;
+    cin >> x;
+    string s=to_string(x);
+    cout << 1;
+    string ans((s.length()-1)*2,'0');
+    cout << ans;
+    cout << 1 << endl;
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
