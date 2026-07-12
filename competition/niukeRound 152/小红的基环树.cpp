b61fa4e30fec40e54aba4dc9671912733986a80f
noxargenta <1849743809@qq.com>
2026-07-12 11:23:03 +0000

    2026年7月12日 19:23

--- competition/niukeRound 152/小红的基环树.cpp
+++ competition/niukeRound 152/小红的基环树.cpp
@@ -3,15 +3,25 @@ using namespace std;
 using i64 = long long;
 #define endl '\n'
 #define int long long
+vector<int> G[(int)(2*1e5+2)];
 void solve() {
     int n;
     cin >> n;
     int nn=n;
-    
+    set<int> st;
     while(nn--){
         int x,y;
-        cin >> x >>y;
-
+        cin >> x  >> y;
+        st.insert(x);
+        st.insert(y);
+        G[x].push_back(y);
+        G[y].push_back(x);
+    }
+    for(auto x:st){
+        //cout << G[x].size() << endl;
+        if(G[x].size()!=2){
+            cout << x << " ";
+        }
     }
 }
 
