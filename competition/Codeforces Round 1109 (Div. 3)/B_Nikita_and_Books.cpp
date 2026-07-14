f4b63dcbf858f24285de286ca8f04208dcf9b7fd
noxargenta <1849743809@qq.com>
2026-07-14 14:57:58 +0000

    2026年7月14日 22:57

---
+++ competition/Codeforces Round 1109 (Div. 3)/B_Nikita_and_Books.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    int n;
+    cin  >> n;
+    vector<int> a(n+1);
+    int sum=0;
+    int ok=1;
+    for(int i=1;i<=n;i++){
+        cin>> a[i];
+        sum+=a[i]-i;
+        if(sum<0){
+            ok=0;
+        }
+    }
+    if(ok){
+        cout << "YES\n";
+    }else {
+        cout << "NO\n";
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
