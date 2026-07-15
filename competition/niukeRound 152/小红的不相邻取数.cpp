494cd84f7d2ee315606e9f0bd0168ed1d3c89333
noxargenta <1849743809@qq.com>
2026-07-15 15:00:01 +0800

    1

--- competition/niukeRound 152/小红的不相邻取数.cpp
+++ competition/niukeRound 152/小红的不相邻取数.cpp
@@ -14,15 +14,15 @@
         for(int i=1;i<=n;i++){
             cin >> b[i];
         }
-        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+        vector<vector<int>> dp(n+2,vector<int>(2,0));
         dp[0][0]=0;
         dp[0][1]=-INF;
         for(int i=1;i<=n;i++){
             int x=a[i];
             int k=b[i];
             if(x<=0){
-                dp[i][0]==max(dp[i-1][0],dp[i-1][1]);
-                dp[i][0]=-INF;
+                dp[i][0]=max(dp[i-1][0],dp[i-1][1]);
+                dp[i][1]=-INF;
             }else {
                 int f0=dp[i-1][0] + (k/2)*x;
                 int f1=dp[i-1][1] + ((k-1)/2)*x;
