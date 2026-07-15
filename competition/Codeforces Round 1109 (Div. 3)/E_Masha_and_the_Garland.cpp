703c25939b522b71bd4b9fb218e45e958ee1b286
noxargenta <1849743809@qq.com>
2026-07-15 13:51:59 +0000

    2026年7月15日 21:51

---
+++ competition/Codeforces Round 1109 (Div. 3)/E_Masha_and_the_Garland.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    int n,q,l,r,k;
+    string s;
+    cin >> n >> q >> s;
+    vector<int> dp(n+1,0);///10010
+    for(int i=0;i<n-1;i++){
+        
+    }
+    while(q--){
+        cin >> l,r,k;
+
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
