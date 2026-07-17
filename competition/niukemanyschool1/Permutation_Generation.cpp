71644c22a8847e14730f671c31a0345625956037
noxargenta <1849743809@qq.com>
2026-07-17 04:47:21 +0000

    2026年7月17日 12:47

--- competition/niukemanyschool1/Permutation_Generation.cpp
+++ competition/niukemanyschool1/Permutation_Generation.cpp
@@ -1,47 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-using ll=long long;
-ll n,k,x;
-ll pre_sum(vector<ll> a){
-    ll len=a.size(),sum=0;
-    for(ll i=0;i<len;i++){
-        for(ll j=i+1;j<len;j++){
-            sum+=(a[j]-a[i]);
-        }
-    }
-    return sum;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    
 }
-ll line(ll ix){
-    return n-1-ix;
-}
-ll slope(ll ix){
-    return ix;
-}
-int main(){
-    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-    cin>>n>>k>>x;
-    vector<ll> a(n,0);
-    for(ll i=0;i<n;i++){
-        cin>>a[i];
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
     }
-    ll w1=pre_sum(a);
-    cout<<w1<<endl;
-    vector<ll> b(n,0);
-    b[k]=x;
-    vector<ll> cub(n,0);
-    ll now=(slope(k)-line(k))*x;
-    for(ll i=0;i<n;i++){
-        cub[i]=(slope(i)-line(i));
-    }
-    for(ll i=0;i<n;i++){
-        cout<<cub[i]<<" ";
-    }
-    ll rl=(n-2)/2;
-    if(k>rl){
-        for(ll i=n-1;i>rl;i--){
-            ll t=
-        }
-    }
-    cout<<endl;
     return 0;
 }
