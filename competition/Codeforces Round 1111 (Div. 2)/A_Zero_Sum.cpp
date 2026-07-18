04253f3f130d2a48e0d96ee965622b81e6992927
noxargenta <1849743809@qq.com>
2026-07-18 14:52:40 +0000

    2026年7月18日 22:52

---
+++ competition/Codeforces Round 1111 (Div. 2)/A_Zero_Sum.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n+1);
+    int sum=0;
+    int b1=0,b2=0;
+    for(int i=1;i<=n;i++){
+        cin >> a[i];
+        sum+=a[i];
+        if(a[i]==a[i-1] && a[i]!=1){
+            if(a[i]==1){
+                b1++;
+            }else {
+                b2++;
+            }
+        }
+    }
+    
+
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
