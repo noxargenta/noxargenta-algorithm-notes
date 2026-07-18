7f34f43d72c6c1d2ba3db614fc502d7585efd2ee
noxargenta <1849743809@qq.com>
2026-07-18 16:03:53 +0000

    2026年7月19日 00:03

--- competition/Codeforces Round 1111 (Div. 2)/C_Inversion_of_a_Subsequence.cpp
+++ competition/Codeforces Round 1111 (Div. 2)/C_Inversion_of_a_Subsequence.cpp
@@ -1,1 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    int n;
+    cin >> n;
+    int a[n],b[n];
+    string ss1,ss2;
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+        ss1+=a[i];
+    }
+    for(int i=0;i<n;i++){
+        cin >> b[i];
+        ss2+=b[i];
+    }
+    if(ss1==ss2){
+        cout << 0 << endl;
+        return;
+    }
+    for(int i=0;i<n;i++){
+        
+    }
+}
 
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
