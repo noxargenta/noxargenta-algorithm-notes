af4f61adc94e4d8a480cc3e50617eb3041e6a3e7
noxargenta <1849743809@qq.com>
2026-07-18 15:36:51 +0000

    2026年7月18日 23:36

--- competition/Codeforces Round 1111 (Div. 2)/B_Yet_Another_Constructive.cpp
+++ competition/Codeforces Round 1111 (Div. 2)/B_Yet_Another_Constructive.cpp
@@ -1,1 +1,33 @@
-cpp
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    int n,k,m;
+    cin >> n >> k >> m;
+    if(k>m){
+        cout << "NO\n";
+    }
+    cout << "YES\n";
+    for(int i=1;i<=n;i++){
+        if(i%k!=0){
+            cout << "1" << " ";
+        }else {
+            cout << m-k+1 << " ";
+        }
+    }
+    cout << endl;
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
