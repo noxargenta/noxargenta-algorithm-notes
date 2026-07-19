af944acd4a66385c670567830575119af3279756
noxargenta <1849743809@qq.com>
2026-07-19 07:40:15 +0000

    2026年7月19日 15:40

--- competition/Codeforces Round 1111 (Div. 2)/C_Inversion_of_a_Subsequence.cpp
+++ competition/Codeforces Round 1111 (Div. 2)/C_Inversion_of_a_Subsequence.cpp
@@ -8,46 +8,38 @@ void solve() {
     cin >> n;
     int a[n],b[n];
     string ss1,ss2;
+    int suma=0,sumb=0;
     for(int i=0;i<n;i++){
         cin >> a[i];
         ss1+=a[i];
+        suma+=a[i];
     }
     for(int i=0;i<n;i++){
         cin >> b[i];
         ss2+=b[i];
+        sumb+=b[i];
     }
     if(ss1==ss2){
         cout << 0 << endl;
         return;
     }
-    int ans=0;
+    int ans=0;int sum = 0;
     for(int i=0;i<n;i++){
         if(a[i]!=b[i]){
-            int sum=a[i];
-            int dif=1;
-            int j;
-            for( j=i+1;j<n;j++){
-                if(a[j]!=b[j]){
-                    sum+=a[j];
-                    dif++;
-                }else {
-                    break;
-                }
-            }
-            i=j-1;
-            if(sum==0){
-                cout << "-1\n";
-                return;
-            }
-            if(sum%2==1){
-                ans++;
-            }else {
-                ans+=2;
-            }
-
+            sum += a[i];
         }
     }
-    cout << ans  << endl;
+    if(suma==0 || sumb == n){
+        cout << "-1\n";
+        return;
+    }
+    if(sum % 2==0){
+        cout << "2\n"; 
+    }else {
+        cout << "1\n";
+    }
+
+    //cout << ans  << endl;
 }
 
 signed main() {
