55cb74e0ccc0d1ca9efa5a99147c5df8e150d66d
noxargenta <1849743809@qq.com>
2026-07-19 11:59:27 +0000

    2026年7月19日 19:59

--- competition/牛客周赛 Round 153/小红的网格图构造_2_0.cpp
+++ competition/牛客周赛 Round 153/小红的网格图构造_2_0.cpp
@@ -11,8 +11,34 @@ void solve() {
         cout << "No\n";
         return;
     }
+    cout << "Yes\n";
     vector<string> a(n,string('0',m));
-    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(i%2==1 && j%2==1){
+                a[i][j]='1';
+                k--;    
+            }
+
+        }
+    }
+    int ok=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(a[i][j]=='0' && k!=0){
+                a[i][j]='1';
+                k--;
+            }
+            if(k==0){
+                ok=1;
+            }
+            if(ok==1)break;
+        }
+        if(ok==1)break;
+    }
+    for(int i=0;i<n;i++){
+        cout << a[i] << endl;
+    }
 }
 
 signed main() {
