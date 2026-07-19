bd53e2dfbfe6d526cb7de1a9c08c15b925aede6c
noxargenta <1849743809@qq.com>
2026-07-19 11:53:23 +0000

    2026年7月19日 19:53

---
+++ competition/牛客周赛 Round 153/小红的网格图构造_2_0.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    int n,m,k;
+    cin >> n >> m >> k;
+    int nn=n/2,mm=m/2;
+    if(nn*mm>k){
+        cout << "No\n";
+        return;
+    }
+    
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
