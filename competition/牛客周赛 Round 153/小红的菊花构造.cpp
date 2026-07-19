c1e711a0d1ab086c877dc52c1352e4e9100f7472
noxargenta <1849743809@qq.com>
2026-07-19 11:07:09 +0000

    2026年7月19日 19:07

---
+++ competition/牛客周赛 Round 153/小红的菊花构造.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    int n,k;
+    cin >> n >> k;
+    for(int i=1;i<n;i++){
+        if(i!=k){
+            cout << k <<" "<< i << endl;
+        }
+    }
+    if(k!=n){
+        cout << k <<" "<< n;
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
