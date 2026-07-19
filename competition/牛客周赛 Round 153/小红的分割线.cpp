6f8a0822efcaccb45b78c5211eaefa0ba3c9b4d7
noxargenta <1849743809@qq.com>
2026-07-19 11:36:05 +0000

    2026年7月19日 19:36

---
+++ competition/牛客周赛 Round 153/小红的分割线.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    int n;
+    cin >> n;
+    vector<pair<int,int>> a(n);
+    for(int i=0;i<n;i++){
+        cin >> a[i].first >> a[i].second;
+    }
+    int ans=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            int l=0,r=0;
+            for(int k=0;k<n;k++){
+                if(k==i || k==j)continue;
+                int cc=
+            }
+        }
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
