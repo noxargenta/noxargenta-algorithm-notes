213922e0c91e4211c1ba711a925560a63634c264
noxargenta <1849743809@qq.com>
2026-07-19 11:04:06 +0000

    2026年7月19日 19:04

---
+++ competition/牛客周赛 Round 153/小红的字符串处理.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    string s;
+    cin >> s;
+    int n=1;
+    for(auto x : s ){
+        if(n==s.length()){
+            cout << x;
+            return;
+        }
+        cout << x << ".";
+        n++;
+    }
+    cout << endl;
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
