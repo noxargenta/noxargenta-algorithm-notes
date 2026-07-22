a5f8515bb9c12301b4cf9944a945c08babc0d5bd
noxargenta <1849743809@qq.com>
2026-07-22 04:20:02 +0000

    2026年7月22日 12:20

---
+++ competition/niukemanyschool2/Bitwise_Maximization.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    int n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin >>  a[i];
+    }
+    int ans=0;
+    int ans2=0;
+    for(int i=0;i<n;i++){
+        if()
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
