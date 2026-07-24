e18d3ffd9f14f20ca1d4ef4ec89dd7a31198023d
noxargenta <1849743809@qq.com>
2026-07-24 04:14:18 +0000

    2026年7月24日 12:14

---
+++ competition/niukemanyschool3/Turn_by_Turn_Navigation.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+int x[100001];
+int y[100001];
+void solve() {
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> x[i] >> y[i];
+
+    }
+    for(int i=1;i<n;i++){
+        int dx=x[i]-x[i-1];
+        int dy=y[i]-y[i-1];
+        int nx=x[i+1]-x[i];
+        int ny=y[i+1]-y[i];
+        int chaji=nx*ny-
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
