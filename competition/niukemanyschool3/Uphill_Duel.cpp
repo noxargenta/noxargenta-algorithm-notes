bf90c4d14ff89177a43fe546f410fe202f69f4e0
noxargenta <1849743809@qq.com>
2026-07-24 04:55:59 +0000

    2026年7月24日 12:55

---
+++ competition/niukemanyschool3/Uphill_Duel.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define int long long
+void solve() {
+    int n,m;
+    cin >> n >> m;
+    int g[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin >> g[i][j];
+        }
+    }
+    
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
