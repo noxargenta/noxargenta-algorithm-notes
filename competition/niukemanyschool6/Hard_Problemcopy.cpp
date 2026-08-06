1e609f6ecee0ad9f23a22b46b616b36c827bf3b3
noxargenta <1849743809@qq.com>
2026-08-06 13:52:08 +0000

    2026年8月6日 21:52

--- competition/niukemanyschool6/Hard_Problemcopy.cpp
+++ competition/niukemanyschool6/Hard_Problemcopy.cpp
@@ -24,7 +24,7 @@ void solve() {
     do{
         bool ok=1;
         for(int i=0;i<n;i++){
-            ll dif=abs(a[i]-a[()]);
+            ll dif=abs(a[i]-a[i%(n-1)]);
             if(isPrime(dif)){
                 ok=0;
             }
