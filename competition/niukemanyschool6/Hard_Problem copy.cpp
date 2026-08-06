896f654c96a8ba3833d5362b9f3d3e49c6af7425
noxargenta <1849743809@qq.com>
2026-08-06 13:40:12 +0000

    2026年8月6日 21:40

--- competition/niukemanyschool6/Hard_Problem copy.cpp
+++ competition/niukemanyschool6/Hard_Problem copy.cpp
@@ -3,8 +3,16 @@ using namespace std;
 using i64 = long long;
 #define endl '\n'
 #define ll long long
-ll chack(vector<ll> a){
-    for()
+bool isPrime(long long n) {
+    if (n < 2) return false;
+    if (n == 2 || n == 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (long long i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 void solve() {
     ll n;
