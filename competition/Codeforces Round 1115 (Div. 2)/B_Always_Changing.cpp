0b2473c6d24b2dd9d1ad4c624d6eaa80f0325359
noxargenta <1849743809@qq.com>
2026-08-06 15:37:42 +0000

    2026年8月6日 23:37

--- competition/Codeforces Round 1115 (Div. 2)/B_Always_Changing.cpp
+++ competition/Codeforces Round 1115 (Div. 2)/B_Always_Changing.cpp
@@ -3,14 +3,24 @@ using namespace std;
 using i64 = long long;
 #define endl '\n'
 #define ll long long
+ll n;string s;ll n0=0;
+    ll n1=0;string ss;
+ll get_len(char x,char y){
+    int ans=ss.length();
+    if(x!=ss.front()){
+        ans--;
+    }
+    if(y!=ss.back()){
+        ans--;
+    }
+}
 void solve() {
-    ll n;
+    
     cin >> n;
-    string s;
+    
     cin >> s;
-    ll n0=0;
-    ll n1=0;
-    string ss;
+    
+    
     for(auto x : s){
         if(x=='0'){
             n0++;
@@ -23,7 +33,10 @@ void solve() {
         cout << -1<<endl;
         return;
     }
-    
+    ll anss=-1;
+    for(ll d=-1;d<2;d++){
+        
+    }
 
 }
 
