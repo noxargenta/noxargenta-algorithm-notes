bb51689d986292e411b209bf0c97401ba0ba63a1
noxargenta <1849743809@qq.com>
2026-08-06 13:39:15 +0000

    2026年8月6日 21:39

---
+++ competition/niukemanyschool6/Hard_Problem copy.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+ll chack(vector<ll> a){
+    for()
+}
+void solve() {
+    ll n;
+    cin >> n;
+    vector<ll> a;
+    for(ll i=1;i<=n;i++){
+        a.push_back(i);
+    } 
+
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
