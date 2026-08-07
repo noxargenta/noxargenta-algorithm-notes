dfb239f1b8402cacda30329cd6f15317eaa243d0
noxargenta <1849743809@qq.com>
2026-08-07 06:57:46 +0000

    2026年8月7日 14:57

--- competition/niukemanyschool7/Bobo_s_Lucky_Modulo.cpp
+++ competition/niukemanyschool7/Bobo_s_Lucky_Modulo.cpp
@@ -26,9 +26,9 @@ signed main() {
 //  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int _ = 1;
+    int _ = 100;
     //cin >> _;
-    while(1) {
+    while(_--) {
         solve();
     }
     return 0;
