9ae8834436d9e25329c616253542a1b647d32c27
noxargenta <1849743809@qq.com>
2026-08-10 10:35:27 +0000

    2026年8月10日 18:35

---
+++ competition/niukemanyschool5/Sequence_Mex_Version.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+void solve() {
+    ll n,k;
+    cin >> n >> k;
+    vector<ll > a(n);
+    for(ll i=0;i<n;i++){
+        cin >> a[i];
+    }
+    
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
