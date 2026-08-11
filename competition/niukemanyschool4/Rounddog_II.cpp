f2613a80bfe070d282a32ba2c10d21ed8104e3a5
noxargenta <1849743809@qq.com>
2026-08-11 13:04:04 +0000

    2026年8月11日 21:04

---
+++ competition/niukemanyschool4/Rounddog_II.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+void solve() {
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    ll t=0;
+    cin >> t;
+    string st="Rounddo"+string('g',t);
+    ll cnt=0;
+    for(ll i=0;i<=s.length()-st.length();i++){
+        if(s.substr(i,st.length())==st){
+            cnt++;
+        }
+    }
+    if(cnt==0){
+        cout << 0<<endl;
+
+    }else if(cnt==1){
+        cout << s.length()-
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
