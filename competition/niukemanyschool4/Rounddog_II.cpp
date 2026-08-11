46fa71d4966f245e4219f553a2e93d6127dc3393
noxargenta <1849743809@qq.com>
2026-08-11 13:27:48 +0000

    2026年8月11日 21:27

--- competition/niukemanyschool4/Rounddog_II.cpp
+++ competition/niukemanyschool4/Rounddog_II.cpp
@@ -15,14 +15,14 @@ void solve() {
     string ss=s+s;
     string st=ini+string(t,'g');
     ll cnt=0;
-    for(ll i=0;i+st.length()<s.length();i++){
+    for(ll i=0;i<s.length() && i+st.length()<ss.length();i++){
         if(ss.substr(i,st.length())==st){
             cnt++;
         }
     }
     if(cnt==0){
         cout << 0<<endl;
-    }else if(cnt==1){
+    }else if(cnt==1){cout << "st.length()" << st.length() <<endl;
         cout << s.length()-st.length()+1 <<endl;
     }else{
         cout << s.length() <<endl;
