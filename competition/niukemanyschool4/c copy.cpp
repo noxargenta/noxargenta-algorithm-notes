f5b9db99745e7a17b7d9d821990713ba4b4d121f
noxargenta <1849743809@qq.com>
2026-08-11 14:26:48 +0000

    2026年8月11日 22:26

--- competition/niukemanyschool4/c copy.cpp
+++ competition/niukemanyschool4/c copy.cpp
@@ -6,11 +6,11 @@ using i64 = long long;
 void solve() {
     ll p;
     cin >> p;
-    for(ll x2=1;x2<p/2;x2++){
+    for(ll x2=1;x2<p;x2++){
         for(ll q=2;q<10000;q++){
             for(ll x1=1;x1<10000;x1++){
                 if(x1*x1%q==p%q && x2*x2%p==q%p){
-                    cout << x1<< " "<<x2 <<" "<< p<<" "<<endl;
+                    cout << x1<< " "<<x2 <<" "<< q<<" "<<endl;
                     return;
                 }
             }
