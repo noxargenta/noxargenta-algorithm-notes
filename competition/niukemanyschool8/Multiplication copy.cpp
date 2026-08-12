ba408e959302b85f5b03dedf9de49fe96fb9ef06
noxargenta <1849743809@qq.com>
2026-08-12 06:56:09 +0000

    2026年8月12日 14:56

---
+++ competition/niukemanyschool8/Multiplication copy.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+void solve() {
+    ll a,b,c;cin >>a >> b >> c;
+    string x=string(a,'3');
+    string y="3"+string(b-1,'0');
+    string x1=x+string(a+b,'9');
+    string x2=x+string(a+b,'')
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
