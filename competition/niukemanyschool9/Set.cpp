c468d56042f506dd2e227804fb76933739af4b0e
noxargenta <1849743809@qq.com>
2026-08-14 04:41:56 +0000

    2026年8月14日 12:41

---
+++ competition/niukemanyschool9/Set.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+void solve() {
+    ll n;
+    cin >> n;
+    ll ct0=0;
+    ll ct1=0;
+    ll a[n];
+    set<ll> st;
+    for(ll i=0;i<n;i++){
+        cin >> a[i];
+        st.insert(a[i]);
+        if(a[i]%2==0){
+            ct0++;
+        }else {
+            ct1++;
+        }
+    }
+    ll ans=0;
+    for(auto x : st){
+        if(x%2==1){
+            
+        }
+    }
+
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
