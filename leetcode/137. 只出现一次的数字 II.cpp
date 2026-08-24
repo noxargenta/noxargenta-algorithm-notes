a6b3c16afc69098d4b1ba0c083e2310d84736a03
noxargenta <1849743809@qq.com>
2026-08-24 09:21:15 +0000

    2026年8月24日 17:21

--- leetcode/137. 只出现一次的数字 II.cpp
+++ leetcode/137. 只出现一次的数字 II.cpp
@@ -1,20 +1,6 @@
-#include <bits/stdc++.h>
-using namespace std;
-using i64 = long long;
-#define endl '\n'
-#define ll long long
-void solve() {
-    
-}
-
-signed main() {
-//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int _ = 1;
-    // cin >> _;
-    while(_--) {
-        solve();
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        
     }
-    return 0;
-}
+};
