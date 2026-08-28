cca65aa8700dcf805c06323b5a56d65c1f62cb19
noxargenta <1849743809@qq.com>
2026-08-28 10:12:01 +0000

    2026年8月28日 18:12

---
+++ study/不重复数字.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+void solve() {
+     ll n;
+    cin >> n;
+    set<ll> s;
+    for(ll i=0;i<n;i++){
+        ll x;
+        cin >> x;
+        s.insert(x);
+    }
+    for(auto x : s){
+        cout << x <<  " "; 
+    }
+    cout <<endl;
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
