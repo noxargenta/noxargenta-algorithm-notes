d3bdde3c1cf6ebab03367ab210b62c0a53cc8fb5
noxargenta <1849743809@qq.com>
2026-08-28 11:43:50 +0000

    2026年8月28日 19:43

---
+++ study/牛客练习赛156/Flower_Rainbow_and_Victory.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+void solve() {
+    ll n;
+    cin >> n;
+    string s1;
+    string s2;
+    cin >> s1 >>s2;
+    ll r=0,f=0,d=0;
+    for(ll i=0;i<n;i++){
+        ll a0=0,ab=0,a1=0,ar=0;
+        if(s1[i]=='0'){
+            a0++;
+        }else {
+            a1++;
+        }
+        if(s2[i]=='B'){
+            ab++;
+        }else {
+            ar++;
+        }
+        if(a0+ab !=0 && a1 + ar !=0){
+            
+        }
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
