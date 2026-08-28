9eb8285b999a64451bd9cc94951e9c19c543ff9d
noxargenta <1849743809@qq.com>
2026-08-28 10:04:32 +0000

    2026年8月28日 18:04

---
+++ study/表达式括号匹配.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+void solve() {
+    string s;
+    cin >> s;
+    ll l=0;
+    ll r=0;
+    for(ll i=0;i<s.length();i++){
+        if(s[i]=='('){
+            l++;
+        }else if(s[i]==')'){
+            r++;
+        }
+        
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
