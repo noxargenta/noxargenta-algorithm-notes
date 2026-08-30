c807114af7406ed99d68c0962c3a67f8d32d11ab
noxargenta <1849743809@qq.com>
2026-08-30 05:01:59 +0000

    2026年8月30日 13:01

---
+++ competition/2026江西八校集训联盟第二场联赛/Seat_numbering.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+void solve() {
+    ll n,m;
+    cin >> n >> m;
+    ll r,c;
+    cin >> r >>c;
+    ll sum=n *m;
+    
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
