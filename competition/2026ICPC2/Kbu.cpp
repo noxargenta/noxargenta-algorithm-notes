07e0e2ca8ee57e47fac681251dde70509aac1558
noxargenta <1849743809@qq.com>
2026-09-13 07:43:04 +0000

    2026年9月13日 15:43

--- competition/2026ICPC2/Kbu.cpp
+++ competition/2026ICPC2/Kbu.cpp
@@ -3,8 +3,22 @@ using namespace std;
 using i64 = long long;
 #define endl '\n'
 #define ll long long
-void solve() {
+ll n;
+vector<ll> a;
+map<ll,ll> cnt;
+ll mex(){
     
+}
+void solve() {
+    cin >> n;
+    a.resize(n);
+    cnt.clear();
+
+    for(ll i=0;i<n;i++){
+        cin >> a[i];
+        cnt[a[i]]++;
+    }
+    ll M=mex();
 }
 
 signed main() {
