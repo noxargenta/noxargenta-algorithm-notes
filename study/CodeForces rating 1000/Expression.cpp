3a20a7fd035a8dec1959bb029e69165576ac72c5
noxargenta <1849743809@qq.com>
2026-09-18 02:22:07 +0000

    2026年9月18日 10:22

---
+++ study/CodeForces rating 1000/Expression.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+void solve() {
+       ll a,b,c;
+       cin >> a >> b >> c;
+       
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
+// 1+2*3=7
+// 1*(2+3)=5
+// 1*2*3=6
+// (1+2)*3=9
