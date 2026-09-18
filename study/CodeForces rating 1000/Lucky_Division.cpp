87938f69e442e1b46fec096ac3585ab790e76a79
noxargenta <1849743809@qq.com>
2026-09-18 01:16:17 +0000

    2026年9月18日 09:16

---
+++ study/CodeForces rating 1000/Lucky_Division.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+void solve() {
+    string s;
+    cin >>s;
+    bool ok=1;
+    for(auto x:s){
+        if(x!='4' && x!='7'){
+            ok=0;
+        }
+    }
+    if(ok){
+        cout << "YES\n";
+    }else {
+        cout << "NO\n";
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
