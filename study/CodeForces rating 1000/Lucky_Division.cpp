5973d122f7f2fbdd7daa8158a6f4541aadf38c45
noxargenta <1849743809@qq.com>
2026-09-18 01:20:56 +0000

    2026年9月18日 09:20

--- study/CodeForces rating 1000/Lucky_Division.cpp
+++ study/CodeForces rating 1000/Lucky_Division.cpp
@@ -3,13 +3,24 @@ using namespace std;
 using i64 = long long;
 #define endl '\n'
 #define ll long long
+ll aa[]={4,7,44,47,74,77,444,447,474,477,744,747,774,777};
 void solve() {
     string s;
     cin >>s;
+    ll a=stoll(s);
     bool ok=1;
     for(auto x:s){
         if(x!='4' && x!='7'){
             ok=0;
+        }
+    }
+    for(auto x : aa){
+        if(x>a){
+            break;
+        }
+        if(a%x==0){
+            ok=1;
+            break;
         }
     }
     if(ok){
