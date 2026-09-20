9ecff56c3ad3c10630644ec391596468029295ab
noxargenta <1849743809@qq.com>
2026-09-20 06:22:46 +0000

    2026年9月20日 14:22

--- study/CodeForces rating 1100/Taxi.cpp
+++ study/CodeForces rating 1100/Taxi.cpp
@@ -4,7 +4,17 @@ using i64 = long long;
 #define endl '\n'
 #define ll long long
 void solve() {
-    
+    ll n;
+    cin >> n;
+    vector<ll> cnt(5,0);
+    for(ll i=0;i<n;i++){
+        ll x;
+        cin >> x;
+        cnt[x]++;
+    }
+    for(ll i=1;i<=4;i++){
+        
+    }
 }
 
 signed main() {
