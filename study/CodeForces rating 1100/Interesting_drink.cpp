a56a651f1a8754602ce89b388edce82ad3981e92
noxargenta <1849743809@qq.com>
2026-09-20 07:18:57 +0000

    2026年9月20日 15:18

---
+++ study/CodeForces rating 1100/Interesting_drink.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+void solve() {
+    ll n;
+    cin >> n;
+    vector<ll> a(n,0);
+    for(ll i=0;i<n;i++){
+        cin  >> a[i];
+    }
+    sort(a.begin(),a.end());
+    ll q;
+    cin >> q;
+    while(q--){
+        ll x;
+        cin >> x;
+        
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
