dd43ad08b197ee0a6b7e194f1c52303fec3d7460
noxargenta <1849743809@qq.com>
2026-09-21 05:37:59 +0000

    2026年9月21日 13:37

---
+++ study/CodeForces rating 1100/Ilya_and_Queries.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+void solve() {
+    string s;
+    cin >> s;
+    vector<ll> a(s.length(),0);
+    vector<ll> pre(s.length(),0);
+    for(ll i=0;i<n;i++){
+        
+    }
+    ll n;
+    cin >> n;
+    while(n--){
+
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
