b462cd3eb293d87dc2a527a0270ce25584eca9d1
noxargenta <1849743809@qq.com>
2026-09-21 14:16:29 +0000

    2026年9月21日 22:16

---
+++ study/CodeForces rating 1100/Путь_Робота.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+ll dx[]={1,0,-1,0};
+ll dy[]={0,-1,0,1};
+void solve() {
+    ll n,m;
+    cin >> n >> m;
+    vector<vector<bool>> vis(n,vector<bool>(m,0));
+    vector<string> a(n);
+    for(ll i=0;i<n;i++){
+        cin >> a[i];
+        
+    }
+    
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
