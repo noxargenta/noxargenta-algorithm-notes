5c8ea882151a6ec3f819358a5d417208eb49cddf
noxargenta <1849743809@qq.com>
2026-09-21 14:24:25 +0000

    2026年9月21日 22:24

--- study/CodeForces rating 1100/Путь_Робота.cpp
+++ study/CodeForces rating 1100/Путь_Робота.cpp
@@ -5,7 +5,7 @@ using i64 = long long;
 #define ll long long
 ll dx[]={1,0,-1,0};
 ll dy[]={0,-1,0,1};
-
+char dd[]={'R','D','L','U'};
 void solve() {
     ll n,m;
     cin >> n >> m;
@@ -15,7 +15,17 @@ void solve() {
         cin >> a[i];
         
     }
-    void dfs[&](ll x,ll y){
+    auto dfs=[&](ll x,ll y){
+        for(ll i=0;i<4;i++){
+            ll x2=dx[i];
+            ll y2=dy[i];
+            if(x2>=0 && x2 <n && y2>=0 && y2 <m){
+                if(!vis[x2][y2] && a[x2][y2]=='*'){
+                    cout << dd[i];
+                    self()
+                }
+            }
+        }
 
     };
 }
