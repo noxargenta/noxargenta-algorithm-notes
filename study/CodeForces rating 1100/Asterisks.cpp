2ac4ab29459a58ba194904126400f06968918389
noxargenta <1849743809@qq.com>
2026-09-21 13:58:17 +0000

    2026年9月21日 21:58

---
+++ study/CodeForces rating 1100/Asterisks.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+void solve() {
+    ll n;
+    cin >> n;
+    for(ll i=1;i<=n;i++){
+        string s=(i,'*');
+        cout << s <<endl;
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
