103f438cf2897ac4259239eb6101422aec6642a1
noxargenta <1849743809@qq.com>
2026-09-21 14:03:28 +0000

    2026年9月21日 22:03

---
+++ study/CodeForces rating 1100/Hexagonal_Numbers.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+void solve() {
+    ll n;
+    cin >> n;
+    ll h=(2*n*n-n);
+    cout << h << endl;
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
