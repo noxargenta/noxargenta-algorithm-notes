e792a0486aa86239e6c1266b3c8df9e3bb06986c
noxargenta <1849743809@qq.com>
2026-09-21 05:40:29 +0000

    2026年9月21日 13:40

--- study/CodeForces rating 1100/Ilya_and_Queries.cpp
+++ study/CodeForces rating 1100/Ilya_and_Queries.cpp
@@ -8,13 +8,20 @@ void solve() {
     cin >> s;
     vector<ll> a(s.length(),0);
     vector<ll> pre(s.length(),0);
-    for(ll i=0;i<n;i++){
-        
+    for(ll i=0;i<s.length()-1;i++){
+        if(s[i]==s[i+1]){
+            a[i+1]++;
+        }
+    }
+    for(ll i=1;i<s.length();i++){
+        pre[i]=pre[i-1]+a[i];
     }
     ll n;
     cin >> n;
     while(n--){
-
+        ll l,r;
+        cin >> l >> r;
+        cout << pre[r]-pre[l-1] <<endl;
     }
 }
 
