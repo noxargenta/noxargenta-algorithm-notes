afaa8a77dc27572c61891e753c61028decc66792
noxargenta <1849743809@qq.com>
2026-09-21 15:22:43 +0000

    2026年9月21日 23:22

--- competition/Codeforces Round 1122 (Div. 3)/C_AND_OR_Sort.cpp
+++ competition/Codeforces Round 1122 (Div. 3)/C_AND_OR_Sort.cpp
@@ -61,8 +61,7 @@ void solve() {
         }
     }
     ll ans=pre1[n];
-    ll fir=s.find('1');
-    for(ll i=fir;i<n;i++){
+    for(ll i=1;i<=n;i++){
         ans=min(ans,pre1[i] + pre0[i+1]);
     }
     cout << ans <<endl;
