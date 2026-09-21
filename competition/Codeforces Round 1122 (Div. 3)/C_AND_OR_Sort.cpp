d4fe69c0aae473e55de7282c41e62ea862080280
noxargenta <1849743809@qq.com>
2026-09-21 15:09:19 +0000

    2026年9月21日 23:09

---
+++ competition/Codeforces Round 1122 (Div. 3)/C_AND_OR_Sort.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+bool check(string s){
+    char now=s[0];
+    for(auto x : s){
+        if(now=='1' && x!='1'){
+            return 0;
+        }
+        if(x=='1'){
+            now='1';
+        }
+    }
+    return 1;
+}
+void solve() {
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    if(check(s)){
+        cout << 0 <<endl;
+    }
+    if(s[0]=='1'){
+        ll ans=0;
+        for(ll i=0;i<n;i++){
+            if(s[i]=='0'){
+                ans++;
+            }
+        }
+        cout << ans <<endl;
+        return;
+    }
+    if(s[n-1]=='0'){
+        ll ans=0;
+        for(ll i=0;i<n;i++){
+            if(s[i]=='1'){
+                ans++;
+            }
+        }
+        cout << ans <<endl;
+        return;
+    }
+
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
