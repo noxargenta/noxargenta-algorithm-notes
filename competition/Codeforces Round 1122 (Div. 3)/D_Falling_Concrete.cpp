e315c8c15ee76cc145a8be4438abe881fd3ce3ac
noxargenta <1849743809@qq.com>
2026-09-22 09:10:24 +0000

    2026年9月22日 17:10

--- competition/Codeforces Round 1122 (Div. 3)/D_Falling_Concrete.cpp
+++ competition/Codeforces Round 1122 (Div. 3)/D_Falling_Concrete.cpp
@@ -1,48 +1,40 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
-
+using i64 = long long;
+#define endl '\n'
+#define ll long long
 void solve() {
-    int n;
+    ll n;
     cin >> n;
-    vector<long long> a(n);
-    vector<long long> c(n);
-    
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-        // 算出每个元素的不变特征值 c[i] = a[i] - (下标)
-        // 题目是 1-indexed，所以减去 (i + 1)
-        c[i] = a[i] - (i + 1);
+    vector<ll> a(n,0);
+    for(ll i=0;i<n;i++){
+        ll x;
+        cin >> x;
+
+        a[i]=x-(i+1);
     }
-
-    // 排序并去重（因为连续序列中每个数只能用一次）
-    sort(c.begin(), c.end());
-    c.erase(unique(c.begin(), c.end()), c.end());
-
-    // 寻找最长的公差为 1 的连续递增子段
-    int max_len = 1;
-    int cur_len = 1;
-    for (size_t i = 1; i < c.size(); ++i) {
-        if (c[i] == c[i - 1] + 1) {
-            cur_len++;
-        } else {
-            cur_len = 1;
+    sort(a.begin(),a.end());
+    a.erase(unique(a.begin(),a.end()),a.end());
+    ll max_l=1;
+    ll cur=1;
+    for(ll i=1;i<a.size();i++){
+        if(a[i]==a[i-1]+1){
+            cur++;
+        }else {
+            cur=1;
         }
-        max_len = max(max_len, cur_len);
+        max_l=max(cur,max_l);
     }
+    cout << max_l <<endl;
+}   
 
-    cout << max_len << "\n";
-}
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    int t;
-    cin >> t;
-    while (t--) {
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    cin >> _;
+    while(_--) {
         solve();
     }
     return 0;
