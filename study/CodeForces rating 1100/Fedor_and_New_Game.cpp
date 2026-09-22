3b28ac1e4e521b64ee0cc0bfeb1f35d8bdfd16e5
noxargenta <1849743809@qq.com>
2026-09-22 12:26:01 +0000

    2026年9月22日 20:26

--- study/CodeForces rating 1100/Fedor_and_New_Game.cpp
+++ study/CodeForces rating 1100/Fedor_and_New_Game.cpp
@@ -4,6 +4,17 @@ using i64 = long long;
 #define endl '\n'
 #define ll long long
 void solve() {
+    ll n,m,k;
+    cin >> n >> m >> k;
+    vector<ll> a(m);
+    for(ll i=0;i<m;i++){
+        cin  >> a[i];
+    }
+    ll val=0;
+    cin >> val;
+    for(ll i=0;i<m;i++){
+        ll now=val^a[i];
+    }
     
 }
 
