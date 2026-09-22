3d4ebf34891f101938dbc2bd0690cffed359f1ab
noxargenta <1849743809@qq.com>
2026-09-22 11:47:35 +0000

    2026年9月22日 19:47

---
+++ study/CodeForces rating 1100/Sum_of_Odd_Integers.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+void solve() {
+    ll n,m;
+    cin >> n >> m;
+    n%=2;
+    m%=2;
+    if(n==m){
+        cout << "YES\n";
+    }else {
+        cout << "NO\n";
+    }
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
