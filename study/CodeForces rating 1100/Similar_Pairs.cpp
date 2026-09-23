8fd3df11045d230c66f77a35ca1238aac1891b85
noxargenta <1849743809@qq.com>
2026-09-23 03:08:02 +0000

    2026年9月23日 11:08

---
+++ study/CodeForces rating 1100/Similar_Pairs.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+#define ll long long
+void solve() {
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    ll ji=0,ou=0;
+    
+    for(ll i=0;i<n;i++){
+        cin >> a[i];
+        if(a[i]%2==0){
+            ou++;
+        }else {
+            ji++;
+        }
+    }
+    if(n%2==1){
+        cout << "NO\n";
+        return; 
+    }
+    if(ji % 2==0 && ou%2==0){
+        cout << "YES\n";
+    }
+
+    
+}
+
+signed main() {
+//  freopen("../data/data.in","r",stdin), freopen("../data/data.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _ = 1;
+    cin >> _;
+    while(_--) {
+        solve();
+    }
+    return 0;
+}
